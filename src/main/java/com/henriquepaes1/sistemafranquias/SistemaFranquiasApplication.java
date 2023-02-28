8fd92f594f658ee3284a6a6c858f0a996c6dcab8
Henrique Paes de Souza <henriquee.paes1@gmail.com>
2023-02-28 18:23:52 -0300

    initial commit spring

---
+++ src/main/java/com/henriquepaes1/sistemafranquias/SistemaFranquiasApplication.java
@@ -0,0 +1,13 @@
+package com.henriquepaes1.sistemafranquias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaFranquiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaFranquiasApplication.class, args);
+	}
+
+}
